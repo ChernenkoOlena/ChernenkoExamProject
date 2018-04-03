debcaa6d4402f1d6de6566da66160c10854049c8
Olena <el.ch.07@gmail.com>
2018-04-03 13:43:40 +0300

    first commit

---
+++ src/main/java/ChernenkoExamProject/App.java
@@ -0,0 +1,13 @@
+package ChernenkoExamProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
